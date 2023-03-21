2029e1dfc0098f05c9f50dda9c93737185c01fbb
falushinaj <119869603+falushinaj@users.noreply.github.com>
2023-03-21 12:02:37 +0100

    Add files via upload
    Решение задачи изменила

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,14 +1,37 @@
 
+// const menuLink = document.querySelectorAll('.menu__link');
+
+// for (let i = 0; i < menuLink.length; i++) {
+//   menuLink[i].addEventListener('click', function(event) {
+//     event.preventDefault(); // отменяем стандартное действие ссылки, чтобы страница не перезагружалась.
+
+//     const subMenu = this.closest('.menu__item').querySelector('.menu_sub'); // ищем элемент-родитель с классом menu_sub
+    
+//     if (subMenu) { 
+//       subMenu.classList.toggle('menu_active');
+//     }
+//   });
+// }
+
+
+
+
 const menuLink = document.querySelectorAll('.menu__link');
 
 for (let i = 0; i < menuLink.length; i++) {
   menuLink[i].addEventListener('click', function(event) {
-    event.preventDefault(); // отменяем стандартное действие ссылки, чтобы страница не перезагружалась.
-
     const subMenu = this.closest('.menu__item').querySelector('.menu_sub'); // ищем элемент-родитель с классом menu_sub
     
-    if (subMenu) { // если меню, то переключаем у него класс menu_active с помощью toggle
-      subMenu.classList.toggle('menu_active');
+    if (subMenu) { // если меню с вложением
+      event.preventDefault(); // запрещаем переход по ссылке
+      subMenu.classList.toggle('menu_active'); // переключаем у него класс menu_active с помощью toggle
+    } else {
+      // для ссылок без подменю не запрещаем переход по ссылке
+      return true;
     }
   });
 }
+
+
+
+
