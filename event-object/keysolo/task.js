12c009fd593128b07c2dcf68be3e159f247fe7af
falushinaj <119869603+falushinaj@users.noreply.github.com>
2023-03-22 12:04:07 +0100

    Add files via upload
    Задача решена

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,17 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    document.addEventListener('keydown', (event) => {
+      const inputSymbol = event.key.toLowerCase();
+      const currentSymbol = this.currentSymbol.textContent.toLowerCase(); 
+      // метод toLowerCase() приводит символы к нижнему регистру перед сравнением
+  
+      if (inputSymbol === currentSymbol) {
+        this.success();
+      } else {
+        this.fail();
+      }
+    });
   }
 
   success() {
